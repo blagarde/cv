a00e1a02c8706fd1ad119cb5c4cda897df9d5275
Baptiste Lagarde <baptiste.lagarde@centraliens.net>
2013-11-20 22:45:45 +1100

    ADD: Resize feature

--- capture.py
+++ capture.py
@@ -7,7 +7,7 @@ from argparse import ArgumentParser
 from datetime import datetime
 
 # Definition of output format
-outsize = WIDTH, HEIGHT = 80, 60
+WIDTH, HEIGHT = 80, 60
 HERE = os.path.dirname(os.path.abspath(__file__))
 DEFAULT_ROOT = os.path.join(HERE, 'data')
 DEFAULT_FMT = 'ppm'
@@ -17,10 +17,11 @@ def now_str():
     return str(datetime.now()).replace(':','').replace(' ','_')
 
 class App(ComputerVisionApp):
-    def __init__(self, folder=DEFAULT_ROOT, gray=True, format=DEFAULT_FMT):
+    def __init__(self, folder=DEFAULT_ROOT, gray=True, format=DEFAULT_FMT, size=None):
         self.folder = folder
         self.channels = 1 if gray else 3
         self.ext = format
+        self.size = size
         folder = os.path.join(folder, now_str()) if folder == DEFAULT_ROOT else folder
         if not os.path.isdir(folder):
             os.makedirs(folder)
@@ -32,12 +33,19 @@ class App(ComputerVisionApp):
         now = now_str()
         filename = now + '.' + self.ext
         outpath = os.path.join(self.folder, filename)
+        if self.size is not None:
+            frame = cv2.resize(frame, self.size)
         cv2.imwrite(outpath, frame)
         print now, '- Captured frame: ', outpath
 
 if __name__ == "__main__":
     parser = ArgumentParser(description="Open a capture device and save frames to a folder")
-    parser.add_argument('-f', '--folder', default=DEFAULT_ROOT, type=str, help="Path to destination folder (must be writable)")
+    parser.add_argument('-o', '--output', default=DEFAULT_ROOT, type=str, help="Path to destination folder (must be writable)")
+    parser.add_argument('-W', '--width', default=WIDTH, type=int, help="Captured image width (pixels)")
+    parser.add_argument('-H', '--height', default=HEIGHT, type=int, help="Captured image height (pixels)")
+    parser.add_argument('-m', '--maxsize', action="store_true", help="Ignore WIDTH and HEIGHT and use the maximum size")
+    parser.add_argument('-f', '--format', type=str, default=DEFAULT_FMT, choices=['ppm', 'jpg', 'png'], help="Output image file format")
     args = parser.parse_args()
-    app = App(args.folder)
+    size = None if args.maxsize else args.width, args.height
+    app = App(args.output, format=args.format, size=size)
     app.main_loop()
